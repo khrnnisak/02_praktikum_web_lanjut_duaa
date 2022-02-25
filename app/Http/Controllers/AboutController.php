1d96bfcfc2ff09bcba6a38e6acc8d2a07a86bb92
khrnnisak <khrnnisak07@gmail.com>
2022-02-26 00:44:18 +0700

    Praktikum 2.2

---
+++ app/Http/Controllers/AboutController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AboutController extends Controller
+{
+    public function index() {
+        return "Nama : Inda Khoirun Nisak <br>
+            Nim : 2041720020";
+    }
+}
